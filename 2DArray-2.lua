ad48ac2a84d61c9e689f1ee5494828563aa874e5
nisidabay <nisidabay@gmail.com>
2025-01-29 19:52:26 +0100

    Modify 2DArray-2.lua

--- 2DArray-2.lua
+++ 2DArray-2.lua
@@ -1,13 +1,50 @@
---
--- The matrix is initialized with given values and specific elements are printed.
--- This module demonstrates the creation and manipulation of a 2D array (matrix) in Lua.
+#!/usr/sbin/lua
+-- The matrix is initialized with given values and specific elements are
+-- printed. This module demonstrates the creation and manipulation of a 2D array
+-- (matrix) in Lua.
 -- @module matrix_example
 
--- Number of rows and columns for the matrix
+-- Matrix Example Module
+-- This module demonstrates the creation and manipulation of a 2D array (matrix) in Lua.
+
+-- Function to validate the elements and size of the matrix
+local function validateMatrix(num_rows, num_cols, values)
+	if #values ~= num_rows * num_cols then
+		print("The matrix doesn't match")
+		os.exit(1)
+	end
+end
+
+-- Function to create and initialize a matrix with given values
+local function createMatrix(num_rows, num_cols, values)
+	local matrix = {}
+	local value_index = 1
+	validateMatrix(num_rows, num_cols, values)
+	for i = 1, num_rows do
+		matrix[i] = {}
+		for j = 1, num_cols do
+			matrix[i][j] = values[value_index]
+			value_index = value_index + 1
+		end
+	end
+
+	return matrix
+end
+
+
+-- Function to print the entire matrix
+local function printMatrix(mat)
+	for i = 1, #mat do
+		for j = 1, #mat[i] do
+			io.write(mat[i][j], " ")
+		end
+		io.write("\n")
+	end
+end
+
+-- Main execution
 local num_rows = 4
 local num_cols = 4
-
--- List of values to populate the matrix
 local values = {
 	"A",
 	"B",
@@ -27,35 +64,14 @@ local values = {
 	"P",
 }
 
--- Initial value index to access elements from the values list
-local value_index = 1
+local matrix = createMatrix(num_rows, num_cols, values)
 
--- Create and initialize the matrix
-local matrix = {} -- create new matrix
-for i = 1, num_rows do
-	matrix[i] = {} -- create new row
-	for j = 1, num_cols do
-		-- current element: row i, column j
-		-- assign current value to element
-		matrix[i][j] = values[value_index] -- assign element to column
-		value_index = value_index + 1 -- move to next letter
-	end
-end
-
--- Print some elements
+-- Print specific elements
+print("Print only some indexes: ")
 print(matrix[1][1]) -- Output: A
 print(matrix[2][4]) -- Output: H
 print(matrix[3][4]) -- Output: L
 
--- Optional: Function to print the entire matrix
-local function printMatrix(mat)
-	for i = 1, #mat do
-		for j = 1, #mat[i] do
-			io.write(mat[i][j], " ")
-		end
-		io.write("\n")
-	end
-end
-
--- Print the entire matrix (uncomment the following line to print the entire matrix)
+-- Print the entire matrix
+print("\nEntire Matrix:")
 printMatrix(matrix)
