5c102025353358ad640376590224dcbb2096d29f
nisidabay <nisidabay@gmail.com>
2025-01-30 19:40:07 +0100

    Add dict_like_behavior.lua

---
+++ dict_like_behavior.lua
@@ -0,0 +1,18 @@
+#!/usr/sbin/lua
+--
+-- Implements dict-like behavior for tables
+
+local myDict = {}
+local metaTable = {
+	__newindex = function(table, key, value)
+		if type(key) == "string" then
+			rawset(table, key, value)
+			print("Added", key, "=", value)
+		else
+			error("Key must be a string")
+		end
+	end,
+}
+setmetatable(myDict, metaTable)
+
+myDict["age"] = 30 -- Output: Added age = 30
