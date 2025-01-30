8e1af52c3046dbf24ddd615b404871d71f9c9181
nisidabay <nisidabay@gmail.com>
2025-01-30 19:39:47 +0100

    Add counter_closure.lua

---
+++ counter_closure.lua
@@ -0,0 +1,17 @@
+#!/usr/sbin/lua
+--
+-- Make a counter closure
+local function makeCounter()
+    local count = 0
+    return function()
+        count = count + 1
+        return count
+    end
+end
+
+-- Call the counter function
+local counter = makeCounter()
+print("Counter Example:")
+print(counter()) -- Output: 1
+print(counter()) -- Output: 2
+
