0547f0b476802ced5bb2f530f54ec7a6fdadf270
nisidabay <nisidabay@gmail.com>
2025-01-30 19:37:24 +0100

    Add accumulator_closure.lua

---
+++ accumulator_closure.lua
@@ -0,0 +1,15 @@
+#!/usr/sbin/lua
+--
+-- Accumulator Closure
+local function makeAccumulator()
+    local total = 0
+    return function(value)
+        total = total + value
+        return total
+    end
+end
+
+local loopAccumulator = makeAccumulator()
+for i = 1, 5 do
+    print("Value=",i , "Accumulator=", loopAccumulator(i))
+end
