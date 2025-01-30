2015c4e6e6b22bfd11e58ed6f22d3c8c67a4f213
nisidabay <nisidabay@gmail.com>
2025-01-30 19:39:37 +0100

    Modify conditional.lua

--- conditional.lua
+++ conditional.lua
@@ -1,15 +1,8 @@
-#!/usr/bin/lua
+#!/usr/sbin/lua
+--
+-- Conditional Example
+-- Simulate the ternary operator in C
 local age = 15
 local legal_age = 16
-
-if age > legal_age then
-    print("You can vote and drink")
-elseif age <legal_age then
-    print("You cannot neither vote nor drink")
-end
-
-
-print("Showing random numbers from 0,20")
-math.randomseed(os.time())
-local r = math.random(0,20)
-print("Random number: "..r)
+local message = (age >= legal_age) and "You can vote and drink" or "You cannot neither vote nor drink"
+print(message)
