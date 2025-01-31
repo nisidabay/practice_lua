dc4ae5fb2ff1a61d3ab4a1550f5d9a3fe658d293
nisidabay <nisidabay@gmail.com>
2025-01-31 09:17:54 +0100

    Add find_max_number.lua

---
+++ find_max_number.lua
@@ -0,0 +1,21 @@
+#!/usr/sbin/lua
+--
+-- Find max number with variadic arguments
+--
+local function find_max(...)
+	local counter = 0
+	local arguments = { ... }
+	local max = arguments[1]
+
+	for i, num in ipairs(arguments) do
+		if num > max then
+			max = num
+			counter = i
+		end
+	end
+
+	return max, counter
+end
+
+local max, counter= find_max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 100, 16, 17, 18, 19, 20)
+print("Max number is " .. max .. " at position " .. counter)
