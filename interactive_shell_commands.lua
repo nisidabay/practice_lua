ccde9fc2f60c0be37e936dbc2e8c685d5a96fba7
nisidabay <nisidabay@gmail.com>
2025-02-01 23:54:23 +0100

    Add interactive_shell_commands.lua

---
+++ interactive_shell_commands.lua
@@ -0,0 +1,18 @@
+#!/usr/sbin/lua
+--
+-- Interactive shell commands
+
+local function run_command(command)
+    local handle = io.popen(command)
+    if handle == nil then
+        return nil
+    end
+    for line in handle:lines() do
+        print("Command output: " .. line)
+        coroutine.yield()  -- Yield after each line of output
+    end
+    handle:close()
+end
+
+local ping_co = coroutine.create(run_command)
+coroutine.resume(ping_co, "ping -c 5 google.com")
