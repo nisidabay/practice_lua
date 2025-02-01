2b6aa2d00dba4e5cefa2de6d826e3b964255bc73
nisidabay <nisidabay@gmail.com>
2025-02-01 23:51:42 +0100

    Add event_driven.lua

---
+++ event_driven.lua
@@ -0,0 +1,18 @@
+#!/usr/sbin/lua
+--
+-- Event-driven programming in Lua
+
+local function watch_directory(path)
+    local handle = io.popen("inotifywait -m -e create,delete " .. path)
+    if handle == nil then
+        return nil
+    end
+    for line in handle:lines() do
+        print("Directory event: " .. line)
+        coroutine.yield()  -- Yield after each event
+    end
+    handle:close()
+end
+
+local watch_co = coroutine.create(watch_directory)
+coroutine.resume(watch_co, "/path/to/watch")
