c61a7e8b6cbf325152ec94089621e87ddde747da
nisidabay <nisidabay@gmail.com>
2025-02-01 23:48:44 +0100

    Add basic.lua

---
+++ basic.lua
@@ -0,0 +1,28 @@
+-- Variable declarations
+local x = 10 -- Integer variable
+local name = "Carlos" -- String variable
+local isAlive = false -- Boolean variable
+local a = nil -- Nil value
+
+-- Print the values of the variables
+print("Integer:", x)
+print("Name:", name)
+print("Is Alive:", isAlive)
+print("Nil value:", a)
+
+-- Arithmetic operations
+local addition = x + 5
+local subtraction = x - 3
+local multiplication = x * 2
+local division = x / 2
+local exponentiation = x ^ 3
+local modulus = x % 3
+
+-- Print arithmetic results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Exponentiation:", exponentiation)
+print("Modulus:", modulus)
+
