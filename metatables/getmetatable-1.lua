a777e4002d90baaa23e5f69ffb2db8804cc358d5
nisidabay <nisidabay@gmail.com>
2025-02-04 10:57:05 +0100

    Modify metatables/getmetatable-1.lua

--- metatables/getmetatable-1.lua
+++ metatables/getmetatable-1.lua
@@ -1,18 +1,22 @@
 #!/usr/bin/env lua
 
 -- Define a table with initial data
-local personTable = { name = "John", age = 30 }
+local personTable = { name = "John", age = 30, city = "Spain" }
 
 -- Create a metatable with custom behaviors
 local customMetatable = {
-    -- Custom __index function to handle missing keys
-    __index = function(_, key)
-        return "Key '" .. key .. "' not found!"
-    end,
-    -- Custom __tostring function to provide a string representation
-    __tostring = function(tbl)
-        return "This is a custom table with name: " .. tbl.name
-    end,
+	-- Custom __index function to handle missing keys
+	__index = function(_, key)
+		return "Key '" .. key .. "' not found!"
+	end,
+	-- Custom __tostring function to provide a string representation
+	__tostring = function(tbl)
+		local result = {}
+		for _, key in ipairs({ "name", "age", "city" }) do
+			table.insert(result, key .. ": " .. tostring(tbl[key]))
+		end
+		return table.concat(result, ", ")
+	end,
 }
 
 -- Set the custom metatable for the personTable
@@ -23,16 +27,14 @@ local retrievedMetatable = getmetatable(personTable)
 
 -- Verify if the retrieved metatable matches the custom one
 if retrievedMetatable == customMetatable then
-    print("Metatable retrieved successfully!")
+	print("Metatable retrieved successfully!")
 else
-    print("Metatable retrieval failed!")
+	print("Metatable retrieval failed!")
 end
 
 -- Access the table using custom behavior defined in the metatable
 print(personTable.name) -- Output: John
 print(personTable.age) -- Output: 30
-print(personTable.city) -- Output: Key 'city' not found!
-
--- Use custom __tostring behavior defined in the metatable
-print(tostring(personTable)) -- Output: This is a custom table with name: John
+print(personTable.city) -- Output: Spain
+print(tostring(personTable)) -- Output: name: John, age: 30, city: Spain
 
