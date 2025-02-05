101116c0aa7a0b9a9af4a4092f728f538647c39f
nisidabay <nisidabay@gmail.com>
2025-02-05 22:45:00 +0100

    Modify coroutines/coroutine-3.lua

--- coroutines/coroutine-3.lua
+++ coroutines/coroutine-3.lua
@@ -1,29 +1,28 @@
 #!/usr/bin/lua
+--
+-- Function to generate numbers using a coroutine
+local function generateNumbers()
+    -- Create a coroutine to yield numbers from 1 to 5
+    local co = coroutine.create(function()
+        for i = 1, 5 do
+            coroutine.yield(i)
+        end
+    end)
 
--- Define a function to get numbers using a coroutine
--- Creating a coroutine inside it using an anonymous function
-local function getNumber()
-	-- Anonymous function to yield numbers from 1 to 5
-	local co = coroutine.create(function()
-		for i = 1, 5 do
-			coroutine.yield(i)
-		end
-	end)
+    -- Collect yielded numbers in a table
+    local yielded_numbers = {}
+    while coroutine.status(co) ~= "dead" do
+        local _, number = coroutine.resume(co)
+        table.insert(yielded_numbers, number)
+    end
 
-	-- Resume the coroutine and store the yielded values in a table
-	local yielded_numbers = {}
-	while coroutine.status(co) ~= "dead" do
-		local _, number = coroutine.resume(co)
-		table.insert(yielded_numbers, number)
-	end
-
-	return yielded_numbers -- Return the table containing all yielded values
+    return yielded_numbers -- Return the table of yielded numbers
 end
 
--- Loop to call "getNumber" 10 times and print the results
-for index = 1, 10 do
-	local numbers = getNumber()
-	for _, number in ipairs(numbers) do
-		print(string.format("index %d: number %d", index, number))
-	end
+-- Main loop to call "generateNumbers" 10 times and print the results
+for iteration = 1, 10 do
+    local numbers = generateNumbers()
+    for _, number in ipairs(numbers) do
+        print(string.format("iteration %d: number %d", iteration, number))
+    end
 end
