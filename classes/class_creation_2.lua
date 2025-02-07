ed6df388973d5c4cbdff0c529ad764f72d0694a5
nisidabay <nisidabay@gmail.com>
2025-02-07 05:55:22 +0100

    Add classes/class_creation_2.lua

---
+++ classes/class_creation_2.lua
@@ -0,0 +1,35 @@
+#!/usr/bin/lua
+--
+-- Define a class called 'Person'
+-- This class represents a person with a name and age.
+-- In Lua, classes are simulated using tables and metatables.
+Person = {}
+Person.__index = Person  -- Set the __index metamethod to allow method access
+
+--- Constructor for the Person class
+-- @param name The name of the person
+-- @param age The age of the person
+-- @return A new instance of the Person class
+-- The constructor function creates a new table (instance) and sets its
+-- metatable to Person.
+-- This allows the instance to access methods defined in the Person table.
+function Person:new(name, age)
+    local instance = setmetatable({}, Person)  -- Create a new instance and set its metatable
+    instance.name = name  -- Assign the name to the instance
+    instance.age = age    -- Assign the age to the instance
+    return instance       -- Return the new instance
+end
+
+--- Method to greet using the person's name
+-- This method prints a greeting message using the instance's name.
+-- The 'self' parameter refers to the instance on which the method is called.
+function Person:greet()
+    print("Hello, I am " .. self.name)
+end
+
+-- Create an instance of the Person class
+-- The 'new' method is called to create a new Person instance with the given
+-- name and age.
+local person1 = Person:new("Alice", 30)
+person1:greet()  -- Output: Hello, I am Alice
+
