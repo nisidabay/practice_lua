3f51754f16742e56510da88b7016dcb6c08fa7f5
nisidabay <nisidabay@gmail.com>
2025-02-07 05:55:13 +0100

    Add classes/class_creation-1.lua

---
+++ classes/class_creation-1.lua
@@ -0,0 +1,38 @@
+#!/usr/bin/lua
+--
+-- Define the MyClass table which will act as a class
+local MyClass = {}
+MyClass.__index = MyClass
+
+-- Creates a new instance of MyClass.
+-- @return a new object
+function MyClass:New()
+	local new_obj = setmetatable({}, self)
+	new_obj._value = nil -- Use a private field to store the value
+	return new_obj
+end
+
+-- Sets the value of the instance with validation.
+-- @param value The value to set.
+function MyClass:setValue(value)
+	if type(value) == "number" then
+		self._value = value
+	else
+		error("Value must be a number")
+	end
+end
+
+-- Gets the value of the instance.
+-- @return The value of the instance.
+function MyClass:getValue()
+	return self._value
+end
+
+-- Create a new instance of MyClass
+local GetNumber = MyClass:New()
+
+-- Set the value of the instance to 100
+GetNumber:setValue(100)
+
+-- Print the value of the instance
+print(GetNumber:getValue())
