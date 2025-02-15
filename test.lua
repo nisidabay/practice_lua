910958b2e52cdb427d7ecf24e1c833d76ea757ba
nisidabay <nisidabay@gmail.com>
2025-02-15 20:22:41 +0100

    Modify test.lua

--- test.lua
+++ test.lua
@@ -1,32 +1,14 @@
 #!/usr/sbin/lua
 --
--- Create a restricted environment
--- This script sets up a sandboxed environment where only specific functions are
--- allowed.
--- The allowed functions in this environment are 'print' and 'tonumber'.
-
-local env = {
-	print = print, -- Allow only 'print'
-	tonumber = tonumber, -- Allow 'tonumber'
-}
-
--- Function to run code in the sandboxed environment
--- @param code: A string containing the Lua code to be executed in the sandbox.
--- @return: The result of the executed code, or an error if the code fails to load or execute.
-local function runSandboxedCode(code)
-	local func, err = load(code, "sandbox", "t", env) -- Load code with custom _ENV
-	if func then
-		return func()
+-- Check if a string is even
+function EvenLen(str)
+	local ret
+	if #str % 2 == 0 then
+		ret = true
 	else
-		error(err)
+		ret = false
 	end
+	return ret
 end
-
--- Run some code in the sandbox
--- The following code is executed within the restricted environment defined above.
--- It demonstrates the allowed and disallowed operations within the sandbox.
-runSandboxedCode([[
-    print("Hello from sandbox!")  -- Works: 'print' is allowed in the sandbox environment
-    print(tonumber("123"))        -- Works: 'tonumber' is allowed in the sandbox environment
-    print(os.date())              -- Error: 'os' is not allowed in the sandbox environment, resulting in an error
-]])
+print(EvenLen("Carlos"))
+print(EvenLen("Carlota"))
