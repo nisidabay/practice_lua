da2973043a63f54af5824b4c402a28efd0826b7f
nisidabay <nisidabay@gmail.com>
2025-02-24 00:17:20 +0100

    Modify test.lua

--- test.lua
+++ test.lua
@@ -1,14 +1,18 @@
 #!/usr/sbin/lua
 --
--- Check if a string is even
-function EvenLen(str)
-	local ret
-	if #str % 2 == 0 then
-		ret = true
-	else
-		ret = false
-	end
-	return ret
-end
-print(EvenLen("Carlos"))
-print(EvenLen("Carlota"))
+-- Testing vim.fn
+local current_file = vim.fn.expand("%:p")
+print(current_file)
+
+local file_name= vim.fn.expand("%")
+print(file_name)
+
+local last_line=vim.fn.line("$")
+print(last_line)
+
+-- local string="ls -a"
+-- local system_call=vim.fn.system(string)
+-- print("system_call" .. system_call)
+
+local bufnr= vim.fn.bufnr()
+print("buffer number ".. bufnr)
